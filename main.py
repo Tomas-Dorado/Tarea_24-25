214c97a86a66b728a9c41a21b63e8ca2327095af
Tomas-Dorado <tdoradog@student.universidadunie.com>
2025-04-01 07:27:28 +0000

    Cambios en main

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from Game import Game
 
-# Crear una instancia de Game y llamar a la función name
 if __name__ == "__main__":
     game = Game()
     game.start()
+    game.spawn_player("Player1")
+    game.spawn_opponent(is_star=True)
+    game.update_opponent()
+    game.update_player()
+    game.player.shoot()
+    game.opponent.shoot()
     game.update()
     game.end_game()
-    game.reset_score()
+    game.reset()
 
 
 
